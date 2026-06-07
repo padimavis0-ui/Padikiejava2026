8830c527b4627cc3188f450d180c0c2d1a7d6533
padimavis0-ui <padimavis0@gmail.com>
2026-06-07 16:28:24 +0000

    Add files via upload

---
+++ Personalinformation.java
@@ -0,0 +1,32 @@
+public class Personalinformation{ 
+
+
+  public static void main(String[] agrs)
+{
+  
+    String fullname   = "Mavis Padi";
+    int    age        = 26;
+    char   gender     = 'F';
+    double height     = 1.75;
+    float  weight     = 68.5f;
+    boolean isStudent = true;
+    long  phoneNumber = 233559218183L;
+    String grade      = "Level 200";
+    String major      = "Computer Networking";
+    double gpa        = 4.14;
+
+
+    System.out.println("Full Name   : " + fullname);
+    System.out.println("Age         : " + age + "year");
+    System.out.println("Gender      : " + gender);
+    System.out.println("Height      : " + height + "m");
+    System.out.println("Weight      : " + weight + "kg");
+    System.out.println("Students    : " + isStudent);
+    System.out.println("Phone Number: " + phoneNumber);
+    System.out.println("Grade/Level : " + grade);
+    System.out.println("Major       : " + major);
+    System.out.println("GPA         : " + gpa);
+  
+   }
+
+}
