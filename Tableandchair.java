d53540520b3e616b3540d2455a5409a247760753
padimavis0-ui <padimavis0@gmail.com>
2026-06-09 18:01:38 +0000

    Add files via upload

---
+++ Tableandchair.java
@@ -0,0 +1,12 @@
+public class Tableandchair {
+    public static void main(String [] agrs){
+        System.out.println("x                                              x");
+        System.out.println("x                                              x");
+        System.out.println("x                                              x");
+        System.out.println("xxxx          xxxxxxxxxxxx                  xxxx");
+        System.out.println("x   x         x          x                  x  x");
+        System.out.println("x   x         x          x                  x  x");
+        System.out.println("x   x         x          x                  x  x");
+        System.out.println("x   x         x          x                  x  x");
+    }
+}
